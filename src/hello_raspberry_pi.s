11e4f94bffdf2214e02b6c7dec83f519eb2c62c6
vajirag <vajirag@gmail.com>
2020-11-07 11:52:35 +0000

    raspberrypi.s fixes

--- src/hello_raspberry_pi.s
+++ src/hello_raspberry_pi.s
@@ -1,10 +1,10 @@
 
-.global_start @program global_start
+.global _start @program global_start
 
 _start:
-    mv  R0, #1          @ here, #1 is an immediate operand, which means it stores as a part of instruction
+    mov R0, #1          @ here, #1 is an immediate operand, which means it stores as a part of instruction
     ldr R1, =banner     @ banner to print, use the lable
-    mov R2, #18         @ length of the banner
+    mov R2, #19         @ length of the banner
     mov R7, #4          @ store system call number in R7 to called in next line
     svc 0               @ software interrupt to call system call 4
 
