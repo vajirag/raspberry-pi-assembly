a2a46f5c87e91cbbb7a743e1b4a579e2db3ee875
Vajira Ginneliya <vajirag@gmail.com>
2020-11-10 20:02:35 +0000

    function print

--- src/func_print.s
+++ src/func_print.s
@@ -1,19 +1,25 @@
 .global _start
 
-@ funtion to add two numbers
+@ funtion to print a string
 @ R0 - address of the string to print
 @ R1 - length of the string
-@ 
 _print:
     push {R4-R12, LR}
     push {R0-R1}
     mov  R0, #1
-    pop  {R1}
-    pop  {R2}
+    pop  {R1}       @ pop parameter 1
+    pop  {R2}       @ pop parameter 2
     mov  R7, #4
     svc 0
     pop {R4-R12, LR}
     bx  LR
+
+@ program exit function
+_exit:
+    push {R4-R12, LR}
+    mov R0, #0
+    mov R7, #1
+    svc 0
 
 _start:
     ldr R0, =msg1
@@ -24,9 +30,7 @@ _start:
     mov R1, #13
     bl  _print
 
-    mov R0, #0
-    mov R7, #1
-    svc 0
+    bl  _exit
 
 .data
 msg1:   .ascii  "Hello\n"
