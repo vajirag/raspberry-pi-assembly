587812566b90df953a618af5da33ec312c6cd8d8
vajirag <vajirag@gmail.com>
2020-11-10 19:59:18 +0000

    function print - fixes

--- src/func_print.s
+++ src/func_print.s
@@ -8,21 +8,25 @@ _print:
     push {R4-R12, LR}
     push {R0-R1}
     mov  R0, #1
-    pop  R1
-    pop  R2
+    pop  {R1}
+    pop  {R2}
     mov  R7, #4
     svc 0
     pop {R4-R12, LR}
-    BX LR
+    bx  LR
 
 _start:
-    mov R0, =msg1
+    ldr R0, =msg1
     mov R1, #6
-    BX _print
+    bl  _print
+
+    ldr R0, =msg2
+    mov R1, #13
+    bl  _print
 
     mov R0, #0
     mov R7, #1
-    svn 0
+    svc 0
 
 .data
 msg1:   .ascii  "Hello\n"
