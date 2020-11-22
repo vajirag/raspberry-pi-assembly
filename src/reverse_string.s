668889d6037dfa789e6fd9e9b63e6b5623136655
Ubuntu <ubuntu@ubuntu.home>
2020-11-22 20:37:31 +0000

    fix reverse_string

--- src/reverse_string.s
+++ src/reverse_string.s
@@ -1,30 +1,33 @@
-.global _start:
+.global _start
 
 
 _start:
     ldr R4, =msg
-    mov R5, [R4]
-    mov R6, #10
+    mov R6, #10   @ loop counter
 
+@ push char to stack
 _repeat_push:
-    mov  R5, [R4]
-    push R5
+    ldr  R5, [R4]
+    push {R5}
     add  R4, #1
     sub  R6, #1
-    cmp  R5, #10
-    ble  _repeat_push
+    cmp  R6, #1
+    bge _repeat_push
 
-    ldr  R4, =msg
+@ pop char from stack
+    mov  R6, #10  @ loop counter
+    ldr  R4, =out
 _repeat_pop:
-    pop  R5
+    pop  {R5}
     str  R5, [R4]
     add  R4, #1
     sub  R6, #1
-    cmp  R5, #10
-    ble  _repeat_pop
+    cmp  R6, #1
+    bge  _repeat_pop
 
+@ print reversed string stored in =out
     mov R0, #1
-    ldr R1, =msg
+    ldr R1, =out
     mov R2, #10 
     mov R7, #4
     svc 0
@@ -34,4 +37,5 @@ _repeat_pop:
     svc 0
 
 .data
-msg  .ascii "012345679"
+msg:  .ascii "0123456789"
+out:  .ascii ".........."
